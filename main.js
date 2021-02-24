4fa7dc30fab6b8c234fd83ca7f716c6c85e38004
ZhyneAmenodin <amenodin.za28@s.msumain.edu.ph>
2021-02-25 00:52:19 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+var level = require("level");
 var db  = level("./student-db", { valueEncoding: 'json'});
+
+
+const prompt = require('prompt-sync')({sigint: true});
+
+ var stId = prompt('Enter ID: ');
+ var fullName = prompt('Enter full Name: ');
+ var stdAge = prompt('enter age:');
+ var stdAddress = prompt('Enter address: ');
+
+acceptStudents(stId, fullName,stdAge, stdAddress);
+
+function acceptStudents(stId, fullName,stdAge){
+    db.put('student', {id: stId ,name: fullName, age: stdAge, address: stdAddress, status: 'applying' }, function(err){  
+        output();
+    })
+
+}
+
+function output(){
+    db.get('student', function(err, student){
+        console.log(student);
+    })
+    }
