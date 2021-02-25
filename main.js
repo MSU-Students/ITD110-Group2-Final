222724061e482150a4e468149b5c383f3932f0d0
Serad, Basam C <58251967+DeJaVu1223@users.noreply.github.com>
2021-02-25 12:40:49 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 var level = require("level");
 var db  = level("./student-db", { valueEncoding: 'json'});
+var status = ['Applying', 'Under Interview', 'Exam Pending', 'Admitted', 'Probationary'];
+
+const prompt = require('prompt-sync')({sigint: true});
+
+ var stId = prompt('Enter ID: ');
+ var fullName = prompt('Enter full Name: ');
+ var stdAge = prompt('enter age: ');
+ var stdAddress = prompt('Enter address: ');
+acceptStudents(stId, fullName,stdAge, stdAddress);
+
+async function acceptStudents(stId, fullName,stdAge, stdAddress){
+    await db.put('student', {id: stId ,name: fullName, age: Number(stdAge), address: stdAddress }, function(err){  
+    })
+     output();
+     scheduleInterview(stId, 'February 26, 2021');
+}
+
+ async function output(){
+     await db.get('student', function(err, student){
+        console.log(student,'\nStatus: '+ status[0]);
+    })
+    }
+async function scheduleInterview(stId, scheduleDate){
+    await db.get(stId, function(err, value){
+        console.log('\nInterview date is: ' + scheduleDate);
+        console.log('Status: ' + status[1]);
+    })
+}
+
